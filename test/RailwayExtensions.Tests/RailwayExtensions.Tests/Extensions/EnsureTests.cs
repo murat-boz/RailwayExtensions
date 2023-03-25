1d4200b390be3589097286214aafc608416898dc
murat-boz <muratboz@hotmail.com>
2023-03-25 15:00:36 +0300

    Added test.

---
+++ test/RailwayExtensions.Tests/RailwayExtensions.Tests/Extensions/EnsureTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using RailwayExtensions.Extensions;
+
+namespace RailwayExtensions.Tests.Extensions
+{
+    public class EnsureTests
+    {
+        [Fact]
+        public void Ensure_Source_Result_Is_Failure_Predicate_Do_Not_Invoked_Expect_Is_Result_Failure()
+        {
+            //Arrange
+            Result sut = Result.Failure("Error");
+
+            //Act
+            Result result = sut.Ensure(() => true, string.Empty);
+
+            //Assert
+            result.Should().Be(sut);
+        }
+    }
+}
