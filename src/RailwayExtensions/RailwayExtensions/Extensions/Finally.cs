af1eae090f89500b6dc56b870e66683253f470ff
murat-boz <muratboz@hotmail.com>
2023-09-11 22:41:37 +0300

    Added Finally and ToResult extension methods.

---
+++ src/RailwayExtensions/RailwayExtensions/Extensions/Finally.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace RailwayExtensions.Extensions
+{
+    public static partial class ResultExtensions
+    {
+        public static T Finally<T>(this Result result, Func<Result, T> func)
+        {
+            return func(result);
+        }
+
+        public static async Task<T> Finally<T>(this Task<Result> resultTask, Func<Result, T> func)
+        {
+            var result = await resultTask;
+
+            return func(result);
+        }
+
+        public static TOut Finally<TIn, TOut>(this Result<TIn> result, Func<Result<TIn>, TOut> func)
+        {
+            return func(result);
+        }
+
+        public static async Task<TOut> Finally<TIn, TOut>(this Task<Result<TIn>> resultTask, Func<Result<TIn>, TOut> func)
+        {
+            var result = await resultTask;
+
+            return func(result);
+        }
+    }
+}
---
+++ src/RailwayExtensions/RailwayExtensions/Extensions/Result/ToResult.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using System;
+
+namespace RailwayExtensions.Extensions
+{
+    public static partial class ResultExtensions
+    {
+        public static Result<T> ToResult<T>(
+            this T dataToBeCreated)
+        {
+            return Result.Ok(dataToBeCreated);
+        }
+
+        public async static Task<Result<T>> ToResultAsync<T>(
+            this T dataToBeCreated)
+        {
+            return Result.Ok(dataToBeCreated);
+        }
+    }
+}
